89a873e61fbffc3bf1467b76559919f0d8cbc4ed
Xin Cheng <xincheng@XinMcPro.local>
2020-10-24 01:27:57 -0400

    update

--- untar_and_fetch.py
+++ untar_and_fetch.py
@@ -1,25 +1,71 @@
 #!/usr/bin/env python3
-#import io
+
+__author__ = "xin cheng"
+
+Usage=\
+'''
+1. extract all *.z within a gaven folder.  (optional)
+2. collect all TARGET_FILES and put them under folder 'moss'
+P.S. Thanks "xb" for his ideas.
+
+Usage: ./untar_and_fetch.py <submit_path> [-t <TARGET_FILES>] [other options]
+
+    <submitted_path>  : is a folder contains all students submitted .z
+    -t <TARGET_FILES> : optional, indicate the files collect to moss
+                        TARGET_FILES i.e. "create.c process.h"
+    --misc  <misc folder name>  : change the default misc folder name 
+    --moss  <moss folder name>  : change the default moss folder name
+    -h / --help       : print usage.
+
+Demo:
+$> ls lab3/
+foo.z
+bar.z
+$> ./untar_and_fetch.py ./lab3
+$> ls lab3/
+moss/
+ |- foo/ : contains target files
+ |- boo/ : contains target files
+misc
+ |- foo/ : contains the tarball and its extracted files
+ |- boo  : contains the tarball and its extracted files 
+ 
+Example:
+$> ./untar_and_fetch.py /lab3
+$> ./untar_and_fetch.py /lab3 -t "main.c create.c send.*.c" 
+'''
+
+
 import os
 import multiprocessing
 import subprocess
 import sys
-import threading
-import traceback
 import concurrent.futures
 
-# target to collect, will be used as regex
+
+# target to collect, each element will be used as a regex pattern
 TARGET_FILES="""
-process.h .*create.*\.c
-startcpumeasure.c, resched.c, clkhandler.c, clkinit.c,  sleep.c, proccpuuse.c
+process.h .*create.*.c
+startcpumeasure.c, resched.c, clkhandler.c,
+clkinit.c,
+sleep.c,
+proccpuuse.c
 """
-
-
+MISC_FOLDER = "./misc"
+MOSS_FOLDER = "./moss"
 
 # The number of concurrent workers equals the number of CPU threads.
 NUM_WORKERS = multiprocessing.cpu_count()
 
 
+def usage(app="./untar_and_fetch.py"):
+    print(Usage.replace("./untar_and_fetch.py", app))
+    return
+
+
+'''
+Terminal Color
+'''
 class Colors:
     DEFAULT = "\x1B[0m"
     RED = "\x1B[31m"
@@ -31,19 +77,22 @@ class Colors:
     WHITE = "\x1B[37m"
 
 
-
+'''
+for student s, move tarball into 'misc' folder and extract
+'''
 def extract_single_gzip(s):
     student = s[:-2]
-    path = "./misc/"+student
+    path = "./%s/%s"%(MISC_FOLDER,student)
     tarball = path+"/"+s
-    #if s[0:2]=="az":
-    #    raise Exception("wocao")
     os.makedirs(path, exist_ok=True)
     os.rename(s, tarball)
+    print("untar -xf %s -C %s"%(s, path))
     subprocess.run(["tar", "-xf", tarball, "-C", path])
     return
 
-
+'''
+for each *.z file, move and extract.
+'''
 def extract_gzip_files():
     num_success = 0
     failed = []
@@ -58,7 +107,7 @@ def extract_gzip_files():
                 num_success += 1
             except Exception as e:
                 failed.append((futures[future], str(e)))
-    print(Colors.MAGENTA+"extract done (into %s/misc/ folder"%os.getcwd()+Colors.DEFAULT)
+    print(Colors.MAGENTA+"extract done (into %s/%s/ folder"%(os.getcwd(), MISC_FOLDER)+Colors.DEFAULT)
     print("Success: %d / %d"%(num_success, len(submission)))
     if failed:
         print("Failures:")
@@ -66,30 +115,41 @@ def extract_gzip_files():
             print(" %s : %s"%e)
     return
 
-
+'''
+for students, collect each target files
+'''
 def collect_per_student(student, targets):
-    src_path = './misc/'+student
-    dst_path = './moss/'+student
+    src_path = './%s/%s'%(MISC_FOLDER, student)
+    dst_path = './%s/%s'%(MOSS_FOLDER, student)
     os.makedirs(dst_path, exist_ok=True)
     warning = ""
+    num_find_files = 0
     for t in targets:
-        p = subprocess.Popen(["find", src_path, "-type", "f", "-regex", ".*/"+t], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(["find", src_path, "-type", "f", "-regex", ".*/"+t],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (out,err)=p.communicate()
         found = out.decode().strip().split()
         if not found:
             warning += " %s"%t
         else:
+            num_find_files += len(found)
             for f in found:
                 os.rename(f, dst_path+"/"+f[f.rfind("/")+1:])
+
+    print("%s find %d"%(student, num_find_files))
     if warning:
         raise Exception("cannot find '%s'"%warning)
     return
 
+'''
+for all student under 'misc', find target files and move it to 'moss'
+'''
 def collect_targets(targets):
     warning=[]
-    students = os.listdir('./misc')
+    students = os.listdir('./%s'%MISC_FOLDER)
     print("-"*50)
-    print(Colors.MAGENTA+"start collect, find %d students folders under "%len(students) + Colors.YELLOW+"'%s/misc/'"%os.getcwd()+Colors.DEFAULT)
+    print(Colors.MAGENTA+"start collect, find %d students folders under "%len(students) \
+          + Colors.YELLOW+"'%s/%s/'"%(os.getcwd(), MISC_FOLDER)+Colors.DEFAULT)
     with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
         futures = {executor.submit(collect_per_student, s, targets):s for s in students }
         for future in concurrent.futures.as_completed(futures):
@@ -97,25 +157,47 @@ def collect_targets(targets):
                 future.result()
             except Exception as e:
                 warning.append((futures[future], str(e)))
-    print(Colors.MAGENTA+"collected result stored in "+Colors.YELLOW+"'%s'"%(os.getcwd()+"/moss/")+Colors.DEFAULT)
+    print(Colors.MAGENTA+"collected result stored in "+Colors.YELLOW+"'%s/%s/'"%(os.getcwd(), MOSS_FOLDER)+Colors.DEFAULT)
     if warning:
         for e in sorted(warning):
             print(Colors.RED + " %s : %s"%e+Colors.DEFAULT)
 
     return
 
+
 def main(args):
-    if len(args) != 2:
+    global TARGET_FILES, MISC_FOLDER, MOSS_FOLDER
+    if len(args) < 2:
+        usage(args[0])
         print(Colors.RED + "Usage: %s turnin_path" % args[0] + Colors.DEFAULT, file=sys.stderr)
         sys.exit(1)
     elif not os.path.isdir(args[1]):
         print(Colors.RED + "Error: \"%s\" is not a valid path." % args[1] + Colors.DEFAULT, file=sys.stderr)
         sys.exit(1)
+
+    i = 2
+    while i<len(args):
+        cmd = args[i]
+        i+=1
+        if cmd == "-t":
+            TARGET_FILES = args[i]
+            i += 1
+            continue
+        if cmd == "--misc":
+            MISC_FOLDER = args[i]
+            i += 1
+            continue
+        if cmd == "--moss":
+            MOSS_FOLDER = args[i]
+            i += 1
+            continue
+        if cmd == "-h" or cmd == "--help":
+            usage()
+            sys.exit(1)
+        raise Exception("unknown input parameters '%s'"%cmd)
+
     print(Colors.MAGENTA + 'Running %d threads in parallel...' % NUM_WORKERS + Colors.DEFAULT)
-
-    path = args[1]
-    os.chdir(path)
-
+    os.chdir(args[1])
     extract_gzip_files()
     collect_targets(TARGET_FILES.strip().replace(","," ").split())
     return
